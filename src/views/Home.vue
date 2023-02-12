7539ebc055ac0718d4ec814009838e7ee501cad5
Eugene-Eremin <duser20102016@gmail.com>
2023-02-13 01:21:58 +0500

    test vuetify-app

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-combobox v-model="select" :items="items" label="Группы" multiple outlined dense></v-combobox>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-btn @click="getSelected" color="accent" elevation="14">Получить выбранные группы</v-btn>
+</template>
+
+<script>
+export default {
+  // Option API
+  data() {
+    return {
+      select: [],
+      items: [
+        'фыва',
+        'Design',
+        'Vue',
+        'Vuetify',
+      ],
+    }
+  },
+  methods: {
+    getSelected() {
+      console.log(this.select) 
+    }
+  },
+  // Composition API
+  setup() {
+    const getSelectedComposAPI = () => {
+      console.log('Hi')
+    }
+
+    return { getSelectedComposAPI };
+  },
+}
+
+
+</script>
